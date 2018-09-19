73bcb4caea12e241980976df25b93b37051700d9
Dabit <dabit@8c85901b6e1c.ant.amazon.com>
2018-09-19 15:10:10 -0700

    updated readme

---
+++ README.md
@@ -0,0 +1,138 @@
+# AppSync React Translator
+
+Text to audio translation using AWS AppSync, React, Amazon Polly, Amazon Translate, & AWS Lambda
+
+![](https://i.imgur.com/EXZVsXR.jpg)
+
+## Getting started
+
+1. Clone the project & change into the new directory
+
+```sh
+git clone https://github.com/dabit3/appsync-web-translator.git
+
+cd appsync-web-translator
+```
+
+2. Initialize a new AWS Amplify project
+
+```sh
+amplify init
+```
+
+3. Add auth, storage, & AppSync services
+
+```sh
+amplify add auth
+
+amplify add api
+
+amplify add storage
+
+amplify add function
+
+amplify push
+```
+
+4. Update the AppSync Schema in your dashboard to the following:
+
+```graphql
+type Query {
+	getTranslatedSentence(sentence: String!, code: String!): TranslatedSentence
+}
+
+type TranslatedSentence {
+	sentence: String!
+}
+```
+
+5. Update the `getTranslatedSentence` resolver to the following:
+
+##### Request mapping template
+```js
+{
+    "version" : "2017-02-28",
+    "operation": "Invoke",
+    "payload": $util.toJson($context.args)
+}
+```
+
+##### Response mapping template
+```js
+$util.toJson($context.result)
+```
+
+6. Add the new Lambda function Update the `getTranslatedSentence` resolver data source.
+
+7. Update the Lambda function code to the following (make sure to replace the bucket name with your bucket name):
+
+```js
+const AWS = require('aws-sdk')
+AWS.config.update({region: 'us-east-2'})
+const uuidV4 = require('uuid/v4')
+const translate = new AWS.Translate();
+const polly = new AWS.Polly();
+const s3 = new AWS.S3({
+  params: {
+    Bucket: '<YOURBUCKETNAME>',
+  }
+})
+
+exports.handler = (event, context, callback) => {
+  // Step 1: translate the text
+  let message = ''
+  const translateParams = {
+    SourceLanguageCode: 'en',
+    TargetLanguageCode: event.code,
+    Text: event.sentence
+  }
+  
+  translate.translateText(translateParams, function (err, data) {
+    if (err) callback(err)
+    message = data.TranslatedText
+
+    const voices = {
+      'es': 'Penelope',
+      'pt': 'Vitoria',
+      'de': 'Vicki',
+      'en': 'Joanna',
+      'fr': 'Celine'
+    }
+
+    const voice = voices[event.code]
+
+    const pollyParams = {
+      OutputFormat: "mp3", 
+      SampleRate: "8000", 
+      Text: message,
+      TextType: "text", 
+      VoiceId: voice
+    };
+
+    // step 2: synthesize the translation into speech  
+    polly.synthesizeSpeech(pollyParams, function(err, data) {
+      if (err) console.log(err, err.stack); // an error occurred
+      else  {
+        let key = uuidV4()
+        const params2 = {
+          Key: key,
+          ContentType: 'audio/mpeg',
+          Body: data.AudioStream,
+          ACL: 'public-read'
+        };
+        s3.putObject(params2, function(err, data) {
+          if (err) {
+            callback('error putting item: ', err)
+          } else {
+            callback(null, { sentence: key })
+          }
+        });
+      }
+    });
+  });  
+};
+
+
+```
+
+8. Add permissions to Lambda role for Rekognition as well as S3
---
+++ src/App.js
@@ -0,0 +1,146 @@
+import React, { Component } from 'react';
+import './App.css';
+import { css } from 'glamor'
+import { API, graphqlOperation } from 'aws-amplify'
+import { withAuthenticator } from 'aws-amplify-react'
+
+const url = "https://s3.us-east-2.amazonaws.com/amplifytranslators3bucket/19b3b75b-caa7-4e3f-bedc-57dae4ab70be";
+
+const buttons = [
+  'French',
+  'German',
+  'Portugese',
+  'Spanish'
+]
+
+const codes = {
+  'French': { name: 'French', code: 'fr' },
+  'German': { name: 'German', code: 'de' },
+  'Portugese': { name: 'Portuguese', code: 'pt' },
+  'Spanish': { name: 'Spanish', code: 'es' }
+}
+
+const GetAudioQuery = `
+  query($sentence: String! $code: String!) {
+    getTranslatedSentence(sentence: $sentence, code: $code) {
+      sentence
+    }
+  }
+`
+
+class App extends Component {
+  state = {
+    play: false, language: 'French', audio: {}, text: '', audioReady: false, fetching: false
+  };
+  
+  componentDidMount() {
+    this.audio = new Audio(url);
+    this.audio.addEventListener("ended", () => {
+      this.setState({ play: false })
+    });
+  }
+
+  setLanguage = l => {
+    this.setState({
+      language: l
+    })
+  }
+
+  translate = async () => {
+    if (this.state.text === '') return
+    this.setState({ fetching: true })
+    const code = codes[this.state.language].code
+
+    const data = {
+      code,
+      sentence: this.state.text
+    }
+    console.log('data: ', data)
+    try {
+      const audioData = await API.graphql(graphqlOperation(GetAudioQuery, data))
+      const key = audioData.data.getTranslatedSentence.sentence
+      const url = `https://s3.us-east-2.amazonaws.com/amplifytranslators3bucket/${key}`
+      this.audio = new Audio(url)
+      this.setState({
+        audioReady: true,
+        fetching: false
+      })
+    } catch (err) {
+      console.log('error hitting graphql: ', err)
+    }
+  }
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  playAudio = () => {
+    this.audio.play()
+  }
+
+  render() {
+    console.log('state: ', this.state)
+    return (
+      <div className="App">
+        <p style={styles.heading}>React Translator</p>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          {
+            buttons.map((b, i) => <p
+            onClick={() => this.setLanguage(b)}
+            style={{...styles.languageButton, ...{ backgroundColor: this.state.language === b ? '#ff9393' : 'red' }}} key={i}>
+              {b}
+            </p>)
+          }
+        </div>
+        <textarea
+          name='text'
+          placeholder="What would you like to translate?"
+          style={styles.textarea}
+          onChange={this.onChange}
+        />
+        <br />
+        {
+          !this.state.fetching && <button {...css(styles.button)} onClick={this.translate}>Translate</button>
+        }
+        <br />
+        {
+          this.state.audioReady && <button {...css(styles.button)} onClick={this.playAudio}>Play Audio</button>
+        }
+      </div>
+    );
+  }
+}
+
+const styles = {
+  button: {
+    width: 300,
+    height: 45,
+    margin: 5,
+    backgroundColor: 'white',
+    color: 'black',
+    border: 'none',
+    outline: 'none',
+    cursor: 'pointer',
+    fontSize: 18,
+    ':hover': {
+      backgroundColor: '#ff9393'
+    },
+  },
+  textarea: {
+    height: 400,
+    width: 475,
+    marginLeft: 40,
+    marginTop: 20,
+    color: 'black',
+    fontSize: 18,
+    padding: 8
+  },
+  languageButton: {
+    padding: '7px 20px', margin: '0px 3px', cursor: 'pointer'
+  },
+  heading: {
+    fontSize: 60
+  }
+}
+
+export default withAuthenticator(App);
